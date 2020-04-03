c525f6f8785f83a9dff4d04cceef2bb365427646
Keturah Howard <keturah.dev@gmail.com>
2020-04-03 11:10:20 -0700

    create migrations

---
+++ Migrations/20200403180833_Initial.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MasterAnimalShelter.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    AnimalId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true),
+                    Personality = table.Column<string>(nullable: true),
+                    PowerStatus = table.Column<int>(nullable: false),
+                    Age = table.Column<int>(nullable: false),
+                    Gender = table.Column<string>(nullable: true),
+                    LookingForSlave = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.AnimalId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Gender", "LookingForSlave", "Name", "Personality", "PowerStatus", "Type" },
+                values: new object[] { 1, 123, "male", true, "Cornelious", "stoic", 75, "turtle" });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Gender", "LookingForSlave", "Name", "Personality", "PowerStatus", "Type" },
+                values: new object[] { 2, 8, "male", true, "Jasper", "mischevious", 38, "cat" });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Gender", "LookingForSlave", "Name", "Personality", "PowerStatus", "Type" },
+                values: new object[] { 3, 2, "female", false, "Shiba", "insightful", 40, "hamster" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+        }
+    }
+}
